bdfa32513ab22e0d94ef299a62fb068f0a4fe0e6
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-27 19:03:53 +0300

    Дз1. Задача 8

---
+++ Dz1_task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите челое положительное число");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+if (num>1)
+{
+    while (count<=num)
+    {
+        if (count%2 == 0)
+        {
+            Console.Write(count);
+            Console.Write(" ");
+        }
+        count = count +1;
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
