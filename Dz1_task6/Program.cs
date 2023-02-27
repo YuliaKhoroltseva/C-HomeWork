457ea773721ae3f6000235a6c2a964b1c1e83328
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-27 19:03:15 +0300

    Дз1. Задача 6

---
+++ Dz1_task6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли оно четным
+
+Console.WriteLine("Введите целое число");
+int number= int.Parse(Console.ReadLine());
+int checknum = number % 2;
+if (checknum == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+     Console.WriteLine("Число нечетное");
+}
