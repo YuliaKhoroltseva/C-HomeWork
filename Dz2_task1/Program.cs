ac1c2ed19144b8fe30143cc6fc4826a214559be3
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-28 13:40:08 +0300

    ДЗ2. Задача 10

---
+++ Dz2_task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+if (num >99 & num <1000)
+{
+    int result = num / 10 % 10;
+    Console.WriteLine($"{num} -> {result}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число, поробуйте снова!");
+}
