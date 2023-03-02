c3c7af931be065cdbc49ae92ea63975a277ea89a
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-03-03 01:34:50 +0300

    Дз6. Задача 41

---
+++ Dz6/Dz6_task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int PlusNum(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i] > 0) count += 1;
+  }
+  return count;
+}
+
+int[] GetArr(int num)
+{
+  int[] arr = new int[num];
+  for (int i = 0; i < num; i++)
+  {
+    arr[i] = GetNum($"Введите число: ");
+  }
+  return arr;
+}
+
+int GetNum(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int number = GetNum("Количество чисел? : ");
+int[] array = GetArr(number);
+Console.Write($"[{String.Join(", ", array)}] -> ");
+int posnum = PlusNum(array);
+Console.WriteLine(posnum);
