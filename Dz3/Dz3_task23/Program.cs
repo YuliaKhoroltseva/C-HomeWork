e1b3119c80b78d3d6b8ca1c598f7007b825de0d3
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-03-02 03:27:07 +0300

    Дз3. Задача 23

---
+++ Dz3/Dz3_task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void ListNum(int num)
+{
+    int count = 2;
+    while (count<=num)
+    {
+       double sqrnum = Math.Pow(count,3);
+       Console.Write($", {sqrnum}");
+       count++;
+    }
+}
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int number = GetNum("Введите число больше 1");
+
+Console.Write($"{number} -> 1");
+
+ListNum(number);
