e8f19369f7911fdb07bf57d9c0dc20188914dca5
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-03-03 02:08:09 +0300

    Дз9. Задача 64

---
+++ Dz9/Dz9_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string Sortnum(int numN)
+{
+  if (numN == 1) return numN.ToString();
+  return(numN + ", " + Sortnum(numN - 1));
+}
+
+int GetNumArr(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine());
+}
+
+int n = GetNumArr("Значение N: ");
+Console.WriteLine($" N = {n} -> {Sortnum(n)}");
