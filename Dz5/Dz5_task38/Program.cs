bc625a2dc4f470e7ef62333a67dbc90980753e10
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-03-03 01:19:46 +0300

    Дз5. Задача 38

---
+++ Dz5/Dz5_task38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double Minmax (double[] array, bool yes)
+{
+  int length = array.Length;
+  double minmax = array[0];
+  for (int i = 0; i < length; i++)
+  {
+    if (array[i] > minmax & yes == true || array[i] < minmax & yes == false)
+    {
+      minmax = array[i];
+    }
+  }
+  return minmax;
+}
+
+double GetNumArray(string text)
+{
+  Console.Write(text);
+  return double.Parse(Console.ReadLine()!);
+}
+
+double[] FillArray(int number)
+{
+  double[] arr = new double[number];
+  for (int i = 0; i < number; i++)
+  {
+    arr[i] = new Random().Next(0,1000);
+  }
+  return arr;
+}
+
+int GetNumLength(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int num = GetNumLength("Из скольки элементов состоит массив?: ");
+double[] array = FillArray(num);
+Console.Write("[" + String.Join(" ; ", array) + "]");
+double maxElements = Minmax(array, true);
+double minElements = Minmax(array, false);
+Console.Write(" -> " + maxElements + " - " + minElements + " = " + (maxElements - minElements));
+
